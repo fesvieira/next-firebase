1d985e327c2ba8f60b3ea483399cc90f636a605c
Felipe Silva Vieira <65823376+fesvieira@users.noreply.github.com>
2024-01-05 10:43:41 -0300

    chore(UI): small update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import { Inter } from "next/font/google";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import signUp from "@/firebase/auth/signup";
 import React from "react";
+import { Column, Container } from "@/styles/global";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,36 +25,25 @@ function Home() {
     return router.push("/admin");
   };
   return (
-    <div className="wrapper">
-      <div className="form-wrapper">
-        <h1 className="mt-60 mb-30">Sign up</h1>
-        <form onSubmit={handleForm} className="form">
-          <label htmlFor="email">
-            <p>Email</p>
-            <input
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              type="email"
-              name="email"
-              id="email"
-              placeholder="example@mail.com"
-            />
-          </label>
-          <label htmlFor="password">
-            <p>Password</p>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              type="password"
-              name="password"
-              id="password"
-              placeholder="password"
-            />
-          </label>
-          <button type="submit">Sign up</button>
-        </form>
-      </div>
-    </div>
+    <Container>
+      <Column>
+        <button
+          onClick={() => {
+            Router.push("/signup");
+          }}
+        >
+          Sign Up
+        </button>
+
+        <button
+          onClick={() => {
+            Router.push("/signin");
+          }}
+        >
+          Sign In
+        </button>
+      </Column>
+    </Container>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  padding: 1rem;
+
+  height: 100dvh;
+  width: 100%;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 1rem;
+`;
